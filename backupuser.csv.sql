38ef1b98f7346e20177f0247887b252947c2de0a
alejandro907 <91443837+alejandro907@users.noreply.github.com>
2022-05-30 13:53:29 -0500

    Add files via upload

---
+++ backupuser.csv.sql
@@ -0,0 +1,8 @@
+/*
+-- Query: SELECT * FROM integracion_continua.user
+LIMIT 0, 1000
+
+-- Date: 2022-05-28 19:48
+*/
+INSERT INTO `` (`iduser`,`name`,`lastname`) VALUES (1,'Usuario1','Apellido1');
+INSERT INTO `` (`iduser`,`name`,`lastname`) VALUES (2,'Usuario2','Apellido2');
